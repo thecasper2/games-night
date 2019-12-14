3524506c83ede63b6f8c69e1f137438570d021a2
Alex Dolphin <alex.dolphin@trivago.com>
2019-12-14 21:34:06 +0100

    Add fifa MySQL table

--- mysql/players.sql
+++ mysql/players.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS event_players;
 DROP TABLE IF EXISTS event;
 DROP TABLE IF EXISTS player;
+DROP TABLE IF EXISTS fifa_results;
 
 CREATE TABLE player (
     player_id INT NOT NULL AUTO_INCREMENT,
@@ -24,14 +25,33 @@ INSERT INTO event (event_name) VALUES
 ("test_event");
 
 CREATE TABLE event_players (
-   event_id INT NOT NULL,
-   player_id INT NOT NULL,
-   FOREIGN KEY (event_id)  REFERENCES event (event_id)    ON DELETE CASCADE,
-   FOREIGN KEY (player_id) REFERENCES player (player_id) ON DELETE CASCADE,
-   PRIMARY KEY (event_id, player_id)
+    event_id INT NOT NULL,
+    player_id INT NOT NULL,
+    FOREIGN KEY (event_id)  REFERENCES event (event_id) ON DELETE CASCADE,
+    FOREIGN KEY (player_id) REFERENCES player (player_id) ON DELETE CASCADE,
+    PRIMARY KEY (event_id, player_id)
 );
 
 INSERT INTO event_players (event_id, player_id) VALUES 
 (1, 1),
 (1, 2);
 
+CREATE TABLE fifa_results (
+    match_id INT NOT NULL AUTO_INCREMENT,
+    event_id INT NOT NULL,
+    home_player_id INT NOT NULL,
+    away_player_id INT NOT NULL,
+    home_goals INT NOT NULL,
+    away_goals INT NOT NULL,
+    FOREIGN KEY (event_id)  REFERENCES event (event_id) ON DELETE CASCADE,
+    FOREIGN KEY (home_player_id) REFERENCES player (player_id) ON DELETE CASCADE,
+    FOREIGN KEY (away_player_id) REFERENCES player (player_id) ON DELETE CASCADE,
+    PRIMARY KEY (match_id, event_id)
+);
+
+INSERT INTO fifa_results (event_id, home_player_id, away_player_id, home_goals, away_goals) VALUES
+(1, 1, 1, 0, 0),
+(1, 1, 2, 0, 0),
+(1, 1, 2, 0, 0),
+(1, 2, 2, 0, 0),
+(1, 2, 1, 0, 0);
