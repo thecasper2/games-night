d3856f98d47577125c2c0e91b8da346573f65eb2
Alex Dolphin <alex.dolphin@trivago.com>
2019-12-14 22:09:05 +0100

    Get R to read in results from all results tables

--- app/get_data.R
+++ app/get_data.R
@@ -1,5 +1,14 @@
 source("mysql.R")
 
+# Generic data
 df_players <- query("select * from player;")
 df_events <- query("select * from event;")
 df_event_players <- query("select * from event_players;")
+
+# Games data
+games <- c("fifa", "rps", "headers_and_volleys", "ticket_to_ride", "catan", "ctr")
+results <- list()
+
+for(game in games){
+    results[[game]] <- "select * from {game}_results;" %>% glue %>% query
+}
