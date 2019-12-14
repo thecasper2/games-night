8f2dc67f36e54807c8209d058e79f751289cb6da
Alex Dolphin <alex.dolphin@trivago.com>
2019-12-14 21:59:36 +0100

    Add new results tables

--- mysql/players.sql
+++ mysql/players.sql
@@ -2,6 +2,11 @@ DROP TABLE IF EXISTS event_players;
 DROP TABLE IF EXISTS event;
 DROP TABLE IF EXISTS player;
 DROP TABLE IF EXISTS fifa_results;
+DROP TABLE IF EXISTS rps_results;
+DROP TABLE IF EXISTS headers_and_volleys_results;
+DROP TABLE IF EXISTS ticket_to_ride_results;
+DROP TABLE IF EXISTS catan_results;
+DROP TABLE IF EXISTS ctr_results;
 
 CREATE TABLE player (
     player_id INT NOT NULL AUTO_INCREMENT,
@@ -10,19 +15,12 @@ CREATE TABLE player (
     PRIMARY KEY (player_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO player (first_name, last_name) VALUES 
-('Alex', 'Dolphin'),
-('Tom', 'Ayre');
-
 CREATE TABLE event (
     event_id INT NOT NULL AUTO_INCREMENT,
     creation_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     event_name VARCHAR(100) NOT NULL,
     PRIMARY KEY (event_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-INSERT INTO event (event_name) VALUES 
-("test_event");
 
 CREATE TABLE event_players (
     event_id INT NOT NULL,
@@ -31,10 +29,6 @@ CREATE TABLE event_players (
     FOREIGN KEY (player_id) REFERENCES player (player_id) ON DELETE CASCADE,
     PRIMARY KEY (event_id, player_id)
 );
-
-INSERT INTO event_players (event_id, player_id) VALUES 
-(1, 1),
-(1, 2);
 
 CREATE TABLE fifa_results (
     match_id INT NOT NULL AUTO_INCREMENT,
@@ -49,9 +43,56 @@ CREATE TABLE fifa_results (
     PRIMARY KEY (match_id, event_id)
 );
 
-INSERT INTO fifa_results (event_id, home_player_id, away_player_id, home_goals, away_goals) VALUES
-(1, 1, 1, 0, 0),
-(1, 1, 2, 0, 0),
-(1, 1, 2, 0, 0),
-(1, 2, 2, 0, 0),
-(1, 2, 1, 0, 0);
+CREATE TABLE rps_results (
+    match_id INT NOT NULL,
+    event_id INT NOT NULL,
+    home_player_id INT NOT NULL,
+    away_player_id INT NOT NULL,
+    home_wins INT NOT NULL,
+    away_wins INT NOT NULL,
+    FOREIGN KEY (event_id)  REFERENCES event (event_id) ON DELETE CASCADE,
+    FOREIGN KEY (home_player_id) REFERENCES player (player_id) ON DELETE CASCADE,
+    FOREIGN KEY (away_player_id) REFERENCES player (player_id) ON DELETE CASCADE,
+    PRIMARY KEY (match_id, event_id, home_player_id, away_player_id)
+);
+
+CREATE TABLE headers_and_volleys_results (
+    match_id INT NOT NULL,
+    event_id INT NOT NULL,
+    player_id INT NOT NULL,
+    position INT NOT NULL,
+    FOREIGN KEY (event_id)  REFERENCES event (event_id) ON DELETE CASCADE,
+    FOREIGN KEY (player_id) REFERENCES player (player_id) ON DELETE CASCADE,
+    PRIMARY KEY (match_id, event_id, player_id)
+);
+
+CREATE TABLE ticket_to_ride_results (
+    match_id INT NOT NULL,
+    event_id INT NOT NULL,
+    player_id INT NOT NULL,
+    position INT NOT NULL,
+    FOREIGN KEY (event_id)  REFERENCES event (event_id) ON DELETE CASCADE,
+    FOREIGN KEY (player_id) REFERENCES player (player_id) ON DELETE CASCADE,
+    PRIMARY KEY (match_id, event_id, player_id)
+);
+
+CREATE TABLE catan_results (
+    match_id INT NOT NULL,
+    event_id INT NOT NULL,
+    player_id INT NOT NULL,
+    position INT NOT NULL,
+    victory_points INT NOT NULL,
+    FOREIGN KEY (event_id)  REFERENCES event (event_id) ON DELETE CASCADE,
+    FOREIGN KEY (player_id) REFERENCES player (player_id) ON DELETE CASCADE,
+    PRIMARY KEY (match_id, event_id, player_id)
+);
+
+CREATE TABLE ctr_results (
+    match_id INT NOT NULL,
+    event_id INT NOT NULL,
+    player_id INT NOT NULL,
+    position INT NOT NULL,
+    FOREIGN KEY (event_id)  REFERENCES event (event_id) ON DELETE CASCADE,
+    FOREIGN KEY (player_id) REFERENCES player (player_id) ON DELETE CASCADE,
+    PRIMARY KEY (match_id, event_id, player_id)
+);
